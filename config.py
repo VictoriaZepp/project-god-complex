7ce4754827102855583913fec938ea5e13fb23dd
VictoriaZepp <VictoriaZepp@users.noreply.github.com>
2025-11-10 17:45:24 +0100

    added comment to config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ height = 3      #Anzahl rows (y)
 #so viele Tage
 days = 3
 
-#Wahrscheinlichkeiten
-p_sun = 0.5     
-p_rain = 0.4    
+#Wahrscheinlichkeiten zwischen 0-1 (nicht 0-100), weil random modul Werte zwischen 0 und 1 zurück gibt
+p_sun = 0.5                 #=50% sun
+p_rain = 0.4                #=40% rain
 
 #Wenn sun = True reduziert es die Wahrscheinlichkeit, dass es regnet
 #Beispiel: p_rain=0.4 und rain_reduction_when_sun=0.2 -> Regenchance = 0.4*(1-0.2)=0.32
