91a8436cb9046d663384220baa8fe1e7fa47b9e2
VictoriaZepp <VictoriaZepp@users.noreply.github.com>
2025-11-10 13:16:18 +0100

    deletet Testing for debugging

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ def main():
     for day in range(1, config.days +1):            #Startet hier schon mit 1 und nicht mit 0 
         world.start_new_day(grid)
         world.simulate_day(grid, rng)
-
-        y, x = h // 2, w // 2
-        cell = grid[y][x]
-        nbrs = cells.get_neighbors(y, x, h, w)
-        print(f"Day {day}  cell({y},{x})  sun={cell['sun']} rain={cell['rain']} wet={cell['wet']}")
-        print(f"Neighbors: {nbrs}")
-
         world.simulate_night(grid)
 
 
